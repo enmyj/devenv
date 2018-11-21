47b47aeea23ec2da9a709503aab064971f95443d
Ian Myjer <ian.myjer@elderresearch.com>
2018-11-21 15:38:55 -0500

    Add Brewfile and brew install if not exists

--- computerz/mac/software_install.bash
+++ computerz/mac/software_install.bash
@@ -1,40 +1,14 @@
 # Software that can't be installed with Homebrew:
-# Homebrew (or install using curl??)
-# Microsoft Office (bash install?)
+# Microsoft Office (has a brew install but seems dubious)
 # Logitech Options
 # Xcode
 
-# install homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# install homebrew if not already installed
+if [[ $(which brew) ]]; then 
+    echo "Homebrew Already Installed"
+else 
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi;
 
-# brew cask install non-dev tools
-firefox
-google-chrome
-spectacle
-slack
-flux
-karabiner-elements
-alfred # still requires authentication
-adobe-acrobat-reader
-skype
-vlc
-keybase
-safeincloud-password-manager
-microsoft-office # seems dubious
-
-# brew cask install dev tools
-iterm2 # hacker shit
-docker # whales
-visual-studio-code # the best editor
-atom # github editor
-dbeaver-community # open source sql front end
-anaconda # not sure what python version this installs?
-google-cloud-sdk # corporate shit
-
-# brew normal installs
-git
-vim
-fish
-bash-completion
-bash-git-prompt
-
+# brew install things specified in the Brewfile
+brew bundle --file=./Brewfile
